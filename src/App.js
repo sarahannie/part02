9995b5f439e45bff16723a6ae78b542ff8f8d69c
ndianekwu sarah <sarahndianekwu@gmail.com>
2022-06-01 00:59:09 -0700

    exercise 1.3 done

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import Content from "./Content";
 import Total from "./Total";
 
 const App = () => {
-  // const-definitions
-  const course = "Half Stack application development"
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
